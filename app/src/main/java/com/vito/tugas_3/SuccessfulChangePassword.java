818b0044c7668da5e12e2c8379daae348cd14ca3
shivmega <vito.megataruna@mhs.unsoed.ac.id>
2022-09-27 07:36:43 +0700

    Initial commit

---
+++ app/src/main/java/com/vito/tugas_3/SuccessfulChangePassword.java
@@ -0,0 +1,14 @@
+package com.vito.tugas_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SuccessfulChangePassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_successful_change_password);
+    }
+}
